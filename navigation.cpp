545d2759c584718785a6e5f4db878095805c2998
Robert Bromley <robert@bromley.co.za>
2019-10-05 14:59:44 +0100

    add to velocity

--- navigation.cpp
+++ navigation.cpp
@@ -25,15 +25,17 @@ void Navigation::add_accelerometer(Accelerometer *a)
 double Navigation::get_acceleration()
 {
   double a = 0;
-  for (Accelerometer* accelerometer : this->accelerometers)
+  for (Accelerometer *accelerometer : this->accelerometers)
     a += accelerometer->get_acceleration();
-  return a/this->accelerometers.size();
+  return a / this->accelerometers.size();
 }
 
 double Navigation::get_velocity()
 {
-   /// TASK 8 (PERSON A) ///////////////////////////////////////////////////////
-
+  /// TASK 8 (PERSON A) ///////////////////////////////////////////////////////
+  double a = this->get_acceleration();
+  this->velocity += a * DT; //integrate
+  return this->velocity;
   /// END OF TASK 8A ///////////////////////////////////////////////////////////
 }
 
